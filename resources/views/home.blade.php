61360bb2b87f99beb7dc838b1d4c2cadbc241ac1
Ishaq Kassam <ishaq.kassam@gmail.com>
2020-12-19 10:21:26 +0300

    admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status') == IAP)
+                    @if (session('status') == "IAP")
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
