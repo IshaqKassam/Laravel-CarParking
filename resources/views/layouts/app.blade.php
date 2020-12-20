2e2404120c7f2d133567e7049c866ebfaada9685
Ishaq Kassam <ishaq.kassam@gmail.com>
2020-12-20 14:58:07 +0300

    kassamm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,63 +23,50 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                    <!-- {{__()}} -->
-                </a>
-                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button> -->
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                     <!-- <ul class="navbar-nav mr-auto">
-
-                    </ul>  -->
-
-                    <!-- Right Side Of Navbar -->
-                    
                         <!-- Authentication Links -->
                         @guest
-                        <!-- the login -->
-                            <!-- <li class="nav-item"> -->
-                                <div class="navigation">
+                            <div class="navigation">
                                 <ul class="nav-links">
-                                <li><a style="text-decoration:none"  href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                            <!-- </li> -->
-                            <!-- the register -->
-                            @if (Route::has('register'))
-                                <!-- <li class="nav-item"> -->
-                                    <li><a style="text-decoration:none" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                                <!-- </li> -->
+                                        <li><a style="text-decoration:none "  href="{{ route('login') }}">
+                                            {{ __('Login') }}</a>
+                                        </li>                         
+                                            @if (Route::has('register'))    
+                                        <li><a style="text-decoration:rgb(241, 175, 113)" href="{{ route('register') }}">
+                                            {{ __('Register') }</a>
+                                        </li>                              
                                 </ul>
-                             </div>
-                            @endif
+                            </div>
+                                            @endif
                         @else
-                        <ul class="status">
-                            <li class="nav-item dropdown">
+                <div class="navigation">
+                    <ul class="nav-links">
+                            <li>
                                 <!-- the username -->
-                                <a id="navbarDropdown" class="reg" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                                    <a id="navbarDropdown" class="reg" href="#" role="button" 
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
                                 <!-- the logout -->
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" 
+                                    class="d-none">
                                         @csrf
                                     </form>
+                                    
                                 </div>
                             </li>
                         @endguest
                     </ul>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>
 
         <main class="py-4">
         @include('flash-message')
