8160254759f2fa0f61523c017ebd867c828e203f
Ishaq Kassam <ishaq.kassam@gmail.com>
2020-12-20 21:25:07 +0300

    final

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Checkout;
 use DB;
 use Car;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use App\Http\Controllers\Controller;
+
 
 class CheckoutController extends Controller
 {
@@ -23,27 +27,60 @@ public function store(Request $request)
         ]);
         $show = Checkout::create($validatedData);
         
-        $data = DB::table('cars')
-        ->join('checkouts', 'checkouts.car_plate', '=', 'cars.car_plate')
-        ->select('cars.created_at', 'checkouts.created_at')
+        $timein = DB::table('cars')
+        ->select('cars.created_at')
+        ->where('car_plate', '=', $validatedData)
         ->get();
 
-        $timein = DB::table('cars')->select('cars.created_at')->get();
+        $timeout = DB::table('checkouts')
+        ->select('checkouts.created_at')
+        ->where('car_plate', '=', $validatedData)
+        ->get();
 
-        $timeout = DB::table('checkouts')->select('checkouts.created_at')->get();
+        // $time_in = Carbon::parse($timein)->format('H:i:s');
+        // $time_out = Carbon::parse($timeout)->format('H:i:s');
 
-        $difference = (strtotime($timeout) - strtotime($timein));
-        $diff_in_hrs = floor($difference/3600);
+        // $time_in = Carbon::parse( $timein);
+        // $time_out = Carbon::parse( $timeout);
 
-        if($diff_in_hrs<1){
-                $message = "Pay your money";
+        // $diff =$time_out->diffInSeconds( $time_in);
+
+
+         $difference = (strtotime($timeout) - strtotime($timein));
+       $difference_in_hrs =$difference /60;
+        // $diff_in_hrs = ($difference/3600);
+
+        if( $difference_in_hrs<1){
+                $message = "Parking is free";
         }
         else{
-                $message = "Pay your Dues";
+                $charges = ( $difference_in_hrs - 1)* 50;
+                $message = "Pay: " + $charges;
         }
         
-        return redirect('/car')->with('success',$message);
+        return redirect('/car')->with('success', $message);
 }
+
+//         $timein = (String) $timein;
+//         $timeout = (String) $timeout;
+
+
+// // $to = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $timein);
+// $to = Carbon::createFromFormat('H:i:s', $timein);
+// $from = Carbon::createFromFormat('H:i:s', $timeout);
+// // $from = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i', $timeout);
+
+// $diff_in_min = $from - $to;
+// // $diff_in_minutes = $to->diffInMinutes($from);
+// // $diff_in_min = (string) $diff_in_min;
+
+// // print_r($diff_in_minutes); // Output: 20
+
+//        // $difference = (strtotime($timeout) - strtotime($timein));
+//        // $difference_in_hrs =$difference /60;
+//         // $diff_in_hrs = ($difference/3600);
+
+       
 
 public function index()
 {
