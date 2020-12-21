4f924973a888c08f30659d41061aeb76e238bd95
Ishaq Kassam <ishaq.kassam@gmail.com>
2020-12-21 09:01:25 +0300

    branch deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Car Parking System
+                   Car Parking System test
                 </div>
 
                 <div class="links">
